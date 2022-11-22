a80d517f848d74ceb3c10d25eec0be9b066d907f
Mehemmed898 <114071840+Mehemmed898@users.noreply.github.com>
2022-11-22 14:25:23 +0400

    Add files via upload

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css";
+
+const app = () => {
+    return (
+        <>
+            <h3 className="header">Mehemmed</h3>
+            <p>Now I can create component</p>
+        </>
+    )
+}
+
+export default app
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>SALAMM</div>
+  )
+}
+
+export default Header
